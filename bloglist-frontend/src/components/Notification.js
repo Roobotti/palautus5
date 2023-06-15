b8a0a4dd248e0986a1827f1c2c64cdee6a93a080
Roope <2.r.k.niemi@gmail.com>
2023-06-15 08:27:35 +0300

    valmis

---
+++ bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,23 @@
+const Notification = ({ message, error=true }) => {
+  if (message === null) {
+    return null
+  }
+
+
+  if (error) {
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
+  return (
+    <div className="message">
+      {message}
+    </div>
+  )
+
+}
+
+export default Notification
